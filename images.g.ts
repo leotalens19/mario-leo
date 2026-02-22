1ba309e30140ffc1908cde71a43f9ef5d6042279
leotalens19 <leotalens19@gmail.com>
2026-02-22 11:10:27 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ images.g.ts
@@ -0,0 +1,184 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "mario_jump":return img`
+. . . . 2 2 2 2 2 . . . . . . . 
+. . . . 2 2 2 2 2 2 2 2 . . . . 
+. . . . e e e e d e d . . . . . 
+. . . e e e d e d e d . . . . . 
+. . . e e e d d d d d d d d . . 
+. . . e e e e d d d e e e . . . 
+. . . . . d d d d d d d . . . . 
+. . . . e e e 2 e e e . . . . . 
+. . . e e e e 2 e e 2 e e . . . 
+. . e e e e 2 2 2 2 2 e e . . . 
+. . e e e e 2 2 2 2 2 2 e . . . 
+. . d d d 2 2 1 2 2 1 2 d . . . 
+. . . d d 2 2 2 2 2 2 2 d . . . 
+. . . d 2 2 2 2 2 2 2 2 . . . . 
+. . . . 2 2 2 . . 2 2 2 . . . . 
+. . . e e e e . . e e e e . . . 
+`;
+            case "image2":
+            case "supermario-bloque-de-pantalla":return img`
+1 1 1 1 1 1 1 1 1 1 f 1 1 1 1 1 
+1 1 e e e e e e e e f 1 e e e 1 
+1 1 e e e e e e e e f 1 e e e 1 
+1 1 e e e e e e e e f 1 1 1 1 1 
+1 1 e e e e e e e e f f f f f f 
+1 1 e e e e e e e e f f 1 1 1 1 
+1 1 e e e e e e e e f 1 e e e 1 
+1 1 e e e e e e e e f 1 e e e 1 
+1 1 e e e e e e e e f 1 e e e 1 
+1 1 e e e e e e e e f 1 e e e 1 
+1 1 e e e e e e e f f 1 e e e 1 
+1 1 e e e e e e f f 1 e e e e 1 
+f f f f f e e f f 1 e e e e e 1 
+1 1 1 1 f f f f 1 e e e e e e 1 
+1 e e e 1 f f 1 e e e e e e e 1 
+1 1 1 1 1 1 f 1 1 1 1 1 1 1 1 1 
+`;
+            case "image3":
+            case "bloque-de-paret":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+e e e e e f e e e e f e e e e e 
+e e e e e f e e e e f e e e e e 
+f f f f f f f f f f f f f f f f 
+e e e e f e e e e e e e f e e e 
+e e e e f e e e e e e e f e e e 
+f f f f f f f f f f f f f f f f 
+e f f e e f e e e e f e e e e e 
+e f f e e f e e e e f e e e e e 
+f f f f f f f f f f f f f f f f 
+e e e e f e e e f e e e e f e e 
+e e e e f e e e f e e e e f e e 
+f f f f f f f f f f f f f f f f 
+e e e f e e e f e e e f e e e e 
+e e e f e e e f e e e f e e e e 
+f f f f f f f f f f f f f f f f 
+`;
+            case "image4":
+            case "bloque-suerte":return img`
+f f f f f f f f f f f f f f f f 
+f e e e e e e e e e e e e e e f 
+f e f e e e e e e e e e e f e f 
+f e e e e e e e e e e e e e e f 
+f e e e e e e e e e e e e e e f 
+f e e e e e e e e e e e e e e f 
+f e e e e e e e e e e e e e e f 
+f e e e e e e e e e e e e e e f 
+f e e e e e e e e e e e e e e f 
+f e e e e e e e e e e e e e e f 
+f e e e e e e e e e e e e e e f 
+f e e e e e e e e e e e e e e f 
+f e e e e e e e e e e e e e e f 
+f e f e e e e e e e e e e f e f 
+f e e e e e e e e e e e e e e f 
+f f f f f f f f f f f f f f f f 
+`;
+            case "image6":
+            case "bloque-moneda":return img`
+f f f f f f f f f f f f f f f f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 f 4 4 4 4 4 4 4 4 4 4 f 4 f 
+f 4 4 4 4 e e e e e f 4 4 4 4 f 
+f 4 4 4 e e e f e e e 4 4 4 4 f 
+f 4 4 4 e e f 4 f e e f 4 4 4 f 
+f 4 4 4 4 4 4 4 4 e e f 4 4 4 f 
+f 4 4 4 4 4 4 4 e e f f 4 4 4 f 
+f 4 4 4 4 4 4 4 e e 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 e f 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 e f 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 e f 4 4 4 4 4 f 
+f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+f 4 f 4 4 4 e e f f 4 4 4 f 4 f 
+f 4 4 4 4 4 e e e f 4 4 4 4 4 f 
+f f f f f f f f f f f f f f f f 
+`;
+            case "image5":
+            case "bloque-de-tuberia":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f 6 7 7 6 6 7 7 6 6 7 7 f f f 
+f f 6 7 7 6 6 7 7 6 6 7 7 f f f 
+f f 6 7 7 6 6 7 7 6 6 7 7 f f f 
+f f 6 7 7 6 6 7 7 6 6 7 7 f f f 
+f f 6 7 7 6 6 7 7 6 6 7 7 f f f 
+f f f f f f f f f f f f f f f f 
+f f f 6 7 6 6 6 6 6 6 f f f f f 
+f f f 6 7 6 7 6 6 6 7 f f f f f 
+f f f 6 7 6 7 6 6 6 6 f f f f f 
+f f f 6 7 6 7 6 6 6 7 f f f f f 
+f f f 6 7 6 7 6 6 6 6 f f f f f 
+f f f 6 7 6 7 6 6 6 7 f f f f f 
+`;
+            case "image7":
+            case "mario_corriendo":return img`
+. . . . 2 2 2 2 2 . . . . . . . 
+. . . . 2 2 2 2 2 2 2 2 . . . . 
+. . . . e e e e d e d . . . . . 
+. . . e e e d e d e d . . . . . 
+. . . e e e d d d d d d d d . . 
+. . . e e e e d d d e e e . d . 
+. . . . . d d d d d d d . d d . 
+. . . . e e e 2 e e e . . e e . 
+. . . e e e e 2 e e 2 e e e e . 
+. . e e e e 2 2 2 2 2 e e e e . 
+. . e e e e 2 2 2 2 2 2 . . . . 
+. . d d d 2 2 1 2 2 1 2 . . . . 
+. . . d d 2 2 2 2 2 2 2 . . . e 
+. . e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+. . e 2 2 2 2 . . . 2 2 2 2 2 e 
+. . e . . . . . . . . . . . . . 
+`;
+            case "image8":
+            case "goomba-malo-i-esplota":return img`
+. . . . . . e e . . . . . . . . 
+. . . . . . e e . . . . . . . . 
+. . . . . e e e e . . . . . . . 
+. . . . e e e e e e . . . . . . 
+. . . e f f e e e f e . . . . . 
+. . e d f f e e e f f e . . . . 
+e e e d f d f f d f d e e e e e 
+e e e d d d e e d d d e e e e e 
+. e e e e e e e e e e e e e e e 
+. . e e d d d d d d d e e . . . 
+. . . d d d d d d d d d d . . . 
+. . . d d d d d d d d d d . . . 
+. . . d d d d d d d d d d . . . 
+. . . . f d d d d d d f . . . . 
+. . f f f f d d d d f f f f . . 
+. f f f f f . . . . f f f f f . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
